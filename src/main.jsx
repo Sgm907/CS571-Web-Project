7e69fcc50c150d92a2b82e776268f95297579c25
Steven Mills <sgmills@wisc.edu>
2026-07-08 22:38:19 -0500

    removed temp files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Classroom from './components/Classroom'
+import LandingPage from './pages/LandingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Classroom />
+  <LandingPage />
 )
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState, useRef } from "react";
+import { Button, Container, Form, Row, Col, Pagination } from "react-bootstrap";
+
+const LandingPage = () => {
+    return <div>
+        <h1>One day this will be a real website!</h1>
+    </div>
+
+}
+
+
+export default LandingPage;
