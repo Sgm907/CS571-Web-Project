85852849c4ee836e3b1bb5c3f150a0abf8d65325
Steven Mills <sgmills@wisc.edu>
2026-08-12 19:22:57 -0500

    Attempted imports

--- src/components/misc/LoreData.jsx
+++ src/components/misc/LoreData.jsx
@@ -1,53 +1,61 @@
 import CharacterEnum from "../../enums/CharacterEnum";
+import card1 from "../../img/card1.png"
+import card2 from "../../img/card2.png"
+import card3 from "../../img/card3.png"
+import card4 from "../../img/card4.png"
+import card5 from "../../img/card5.png"
+import card6 from "../../img/card6.png"
+import card7 from "../../img/card7.png"
+import card8 from "../../img/card8.png"
 
 //This file is meant to simulate an API response that contains this additional info
 const LoreData = [{
     key: CharacterEnum.WORLD_1,
     title: "The Kingdom",
     desc: "Once, the kingdom was a place of peace and prosperity, respected throughout the land.  After the Great Split it's fallen far from where it once was.  However, it still stands as one of the last safe places in the world.",
-    imgSrc: "\\assets\\img\\card1.png"
+    imgSrc: card1
 },
 {
     key: CharacterEnum.WORLD_2,
     title: "The Void",
     desc: "No one knows where the Void came from.  Some think it's always been there.  Some think it's the remains of the God's failed attempts at creating life.  All we know is that for centuries creatures of the void have found their way into our world.  However, as most of them appear to be mindless, they never posed much of a threat.  Now, with the Void King controlling them, it seems as if there's nothing that can stop the void from consuming all.",
-    imgSrc: "\\assets\\img\\card2.png"
+    imgSrc: card2
 },
 {
     key: CharacterEnum.MAIN_CHAR,
     title: "The Protagonist",
     desc: "Tasked with preventing the Great Split, you went on an epic journey to gather allies & weapons needed to return the land to peace.  Finally, you arrived at ritual site just in time to watch it's completion.  While you couldn't stop the ritual, you were able to get close enough to steal some of the Void King's power over it.  With the void rapidly consuming the world, you're the only one with a chance to stop it.",
-    imgSrc: "\\assets\\img\\card3.png"
+    imgSrc: card3
 },
 {
     key: CharacterEnum.MAIN_ANT,
     title: "The Antagonist",
     desc: "Originally born to a minor noble family on the outskirts of the kingdom, the man who would become the Void King felt he would never be able to rise above his station.  That all changed when he found a mysterious tome to summon the awful powers of the Void.  For years he grew is cult and ammassed the resources required to summon the Void into our world.  Now, he sits at the heart of the Void, leading it's invasion of our world.",
-    imgSrc: "\\assets\\img\\card4.png"
+    imgSrc: card4
 },
 {
     key: CharacterEnum.ENEMY_3,
     title: "Void Bats",
     desc: "Eldrich creatures from the void. These 'bats' have been seen dotting the skies of the world ever since the Great Split.  While heavily armored on their back, they have much less protection should you manage to get above them.",
-    imgSrc: "\\assets\\img\\card7.png"
+    imgSrc: card7
 },
 {
     key: CharacterEnum.ENEMY_1,
     title: "Wolves",
     desc: "Originally pack animals that lived in the forests surrounding the kingdom, these creatures begun moving closer to the kingdom as food becomes scarcer.  They don't pose much of a threat on their own, but they rarely are alone.",
-    imgSrc: "\\assets\\img\\card5.png"
+    imgSrc: card5
 },
 {
     key: CharacterEnum.MECH_1,
     title: "Void Tunnels",
     desc: "As the Void encroaches farther on our world, the Void King's forces use these tunnels to mvoe quickly across the ruined world.  However, with your stolen power you can reroute these tunnels to your advantage, allowing you to move quickly across the world and avoid the Void King's forces.",
-    imgSrc: "\\assets\\img\\card8.png"
+    imgSrc: card8
 },
 {
     key: CharacterEnum.ENEMY_2,
     title: "Void Cultists",
     desc: "The last remaining vestiges of the Cult of the Void.  These fanatics seek to ensure that the Void King is able to complete his conquest of our world.  They are often seen physically controlling the Void King's forces, and are often found near anything the Void King wishes to protect.",
-    imgSrc: "\\assets\\img\\card6.png"
+    imgSrc: card6
 }
 ]
 export default LoreData;
--- src/pages/LorePage.jsx
+++ src/pages/LorePage.jsx
@@ -4,6 +4,15 @@ import LoreTypes from "../enums/VariantEnum";
 import LoreCard from "../components/cards/LoreCard";
 import Style from "../SiteThemes";
 import CharacterEnum from "../enums/CharacterEnum";
+import card1 from "../img/card1.png"
+import card2 from "../img/card2.png"
+import card3 from "../img/card3.png"
+import card4 from "../img/card4.png"
+import card5 from "../img/card5.png"
+import card6 from "../img/card6.png"
+import card7 from "../img/card7.png"
+import card8 from "../img/card8.png"
+
 
 function LorePage() {
     return (
@@ -11,27 +20,27 @@ function LorePage() {
             <Row id="cards">
                 <Col>
                     <div className="mb-4">
-                        <LoreCard title="The Kingdom" text="The world we're trying to save." imageSrc="\assets\img\card1.png" type={LoreTypes.WORLD} charKey={CharacterEnum.WORLD_1} />
+                        <LoreCard title="The Kingdom" text="The world we're trying to save." imageSrc={card1} type={LoreTypes.WORLD} charKey={CharacterEnum.WORLD_1} />
                     </div>
-                    <LoreCard title="The Void" text="Once there was only the void." imageSrc="\assets\img\card2.png" type={LoreTypes.WORLD} charKey={CharacterEnum.WORLD_2} />
+                    <LoreCard title="The Void" text="Once there was only the void." imageSrc={card2} type={LoreTypes.WORLD} charKey={CharacterEnum.WORLD_2} />
                 </Col>
                 <Col>
                     <div className="mb-4">
-                        <LoreCard title="The Protagonist" text="You" imageSrc="\assets\img\card3.png" type={LoreTypes.ALLY} charKey={CharacterEnum.MAIN_CHAR} />
+                        <LoreCard title="The Protagonist" text="You" imageSrc={card3} type={LoreTypes.ALLY} charKey={CharacterEnum.MAIN_CHAR} />
                     </div>
-                    <LoreCard title="The Antagonist" text="The man who destroyed the world" imageSrc="\assets\img\card4.png" type={LoreTypes.ENEMY} charKey={CharacterEnum.MAIN_ANT} />
+                    <LoreCard title="The Antagonist" text="The man who destroyed the world" imageSrc={card4} type={LoreTypes.ENEMY} charKey={CharacterEnum.MAIN_ANT} />
                 </Col>
                 <Col>
                     <div className="mb-4">
-                        <LoreCard title="Wolves" text="The local wildlife." imageSrc="\assets\img\card5.png" type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_1} />
+                        <LoreCard title="Wolves" text="The local wildlife." imageSrc={card5} type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_1} />
                     </div>
-                    <LoreCard title="Void Cultists" text="Followers of the void." imageSrc="\assets\img\card6.png" type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_2} />
+                    <LoreCard title="Void Cultists" text="Followers of the void." imageSrc={card6} type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_2} />
                 </Col>
                 <Col>
                     <div className="mb-4">
-                        <LoreCard title="Void Bats" text="Flying horrors from the sky." imageSrc="\assets\img\card7.png" type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_3} />
+                        <LoreCard title="Void Bats" text="Flying horrors from the sky." imageSrc={card7} type={LoreTypes.ENEMY} charKey={CharacterEnum.ENEMY_3} />
                     </div>
-                    <LoreCard title="Void Tunnels" text="Cross the ruined world." imageSrc="\assets\img\card8.png" type={LoreTypes.WORLD} charKey={CharacterEnum.MECH_1} />
+                    <LoreCard title="Void Tunnels" text="Cross the ruined world." imageSrc={card8} type={LoreTypes.WORLD} charKey={CharacterEnum.MECH_1} />
                 </Col>
             </Row>
         </Container>
